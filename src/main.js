eb6465c012cc0b26bdf4372b03e7bbff8eed2635
Chanjnint21 <limchanjnint@gmail.com>
2023-06-01 18:02:20 +0700

    register global components

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import ImageSolo from './components/ImageSolo.vue'
+import MainTitle from './components/MainTitle.vue'
 import './assets/main.css'
 
+Vue.component('ImageSolo', ImageSolo)
+Vue.component('MainTitle', MainTitle)
 Vue.config.productionTip = false
 
 new Vue({
@@ -11,3 +15,5 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
